ddbb37d44f1de051dad57aa11cc33c72e8b5bf87
arpit mishra <arpit.mishra.out@gmail.com>
2024-06-19 19:33:23 +0530

    oops continue

--- oops.php
+++ oops.php
@@ -6,6 +6,12 @@
 // When individual object is created, it has all the properties and methods from class but the value for every class will be different for individual objects.
 // you can use instanceof keyword to check whether an object is instance of class.
 // A constructor function is created to initalize the object's properties upon creation of object.
+// Acess Modifiers:-
+// There are three access modifiers:
+
+    // public - the property or method can be accessed from everywhere. This is default
+    // protected - the property or method can be accessed within the class and by classes derived from that class
+    // private - the property or method can ONLY be accessed within the class
 
 // class Fruit{
 //     public $name;
@@ -65,4 +71,44 @@
 // echo $apple->color . "<br>";
 
 
+// Inheritance:- When a class is drived from another class
+// An Inherited class is defined by using the extends keyword
+
+class Fruit{
+    public $name;
+    public $color;
+
+    function __construct($name,$color){
+        $this->name = $name;
+        $this->color = $color;
+    }
+
+    public function intro(){
+        return "Fruit name is {$this->name} and the color is {$this->color}";
+    }
+    
+}
+
+class Strawberry extends Fruit{
+
+    public $weight;
+
+    public function __construct($name,$color,$weight){
+        $this->name = $name;
+        $this->color = $color;
+        $this->weight = $weight;
+    }
+    
+    public function berry(){
+        return "Am I a fruit or a berry.";
+    }
+}
+
+
+$berry1 = new Strawberry('strawberry','pink',38);
+echo $berry1->intro() . "<br>";
+echo $berry1->berry(). "<br>";
+echo $berry1->weight. "<br>";
+echo $berry1->name. "<br>";
+echo $berry1->color. "<br>";
 ?>
